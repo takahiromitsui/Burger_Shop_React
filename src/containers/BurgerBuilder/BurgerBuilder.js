5e2aa67db80dbf971ef207f2ca6fd28fe900d254
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2020-12-18 09:19:09 +0100

    125:Implement a BurgerBuilder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux";
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Buger:Graphical</div>
+        <div>Burger Controls:Add Ingredients</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
