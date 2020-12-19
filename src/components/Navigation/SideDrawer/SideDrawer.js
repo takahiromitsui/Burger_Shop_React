18542c7e41ba13b50f2649c7d0b601c094e2f460
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2020-12-19 10:11:31 +0100

    Change layout function into Class

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,16 +2,21 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NaviagtionItems/NaviagationItems";
 import classes from "./SideDrawer.css";
+import BackDrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Aux";
 const sideDrawer = (props) => {
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <BackDrop show/>
+      <div className={classes.SideDrawer}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 export default sideDrawer;
