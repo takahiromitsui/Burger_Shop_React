6cfb016820a08df3bffd909e599b9033e3805e2c
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2020-12-21 09:39:23 +0100

    Add dammy data to test sending of data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../../axios-orders"
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -78,7 +79,22 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = ()=>{
-    alert("You Continue!");
+    // alert("You Continue!");
+    const order = {
+      ingredients:this.state.ingredients,
+      price:this.state.totalPrice,
+      custmor:{
+        name:'Person1',
+        address:{
+          street:'Teststreet1',
+          zipCode:'12345',
+          country:'Germany'
+        },
+        email:'test@test.com'
+      },
+      deliveryMethod:'fastest',
+    }
+    axios.post('/orders.json');
   }
 
   render() {
