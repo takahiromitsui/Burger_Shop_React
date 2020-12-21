3c4d35ae83e749f450bc07e2cfb3e65b0c5ac98f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2020-12-21 09:48:40 +0100

    181:Sending a Post Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
-import axios from "../../../axios-orders"
+import axios from "../../axios-orders";
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -78,24 +78,27 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   };
 
-  purchaseContinueHandler = ()=>{
+  purchaseContinueHandler = () => {
     // alert("You Continue!");
     const order = {
-      ingredients:this.state.ingredients,
-      price:this.state.totalPrice,
-      custmor:{
-        name:'Person1',
-        address:{
-          street:'Teststreet1',
-          zipCode:'12345',
-          country:'Germany'
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      custmor: {
+        name: "Person1",
+        address: {
+          street: "Teststreet1",
+          zipCode: "12345",
+          country: "Germany",
         },
-        email:'test@test.com'
+        email: "test@test.com",
       },
-      deliveryMethod:'fastest',
-    }
-    axios.post('/orders.json');
-  }
+      deliveryMethod: "fastest",
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
 
   render() {
     const disabledInfo = {
@@ -109,12 +112,16 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary 
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
+          <OrderSummary
             ingredient={this.state.ingredients}
             price={this.state.totalPrice}
             purchaseCanceled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler} />
+            purchaseContinued={this.purchaseContinueHandler}
+          />
         </Modal>
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
