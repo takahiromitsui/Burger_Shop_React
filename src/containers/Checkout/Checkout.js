f35654441ae8177226c44f515de5d5735205b8e1
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2020-12-22 10:10:48 +0100

    266: Navigating to ContactData

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,38 +1,44 @@
-import React, { Component } from 'react';
-import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary"
-class Checkout extends Component{
-  state={
-    ingredients:{
-      salad:1,
-      bacon:1
-    }
-  }
-  componentDidMount(){
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+
+import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import ContactData from "../Checkout/ContactData/ContactData";
+class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+    },
+  };
+  componentDidMount() {
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
-    for (let param of query.entries()){
+    for (let param of query.entries()) {
       ingredients[param[0]] = +param[1];
     }
-    this.setState({ingredients:ingredients})
+    this.setState({ ingredients: ingredients });
   }
-  checkoutCanceledHandler=()=>{
+  checkoutCanceledHandler = () => {
     this.props.history.goBack();
+  };
+  checkoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
 
-  }
-  checkoutContinuedHandler=()=>{
-    this.props.history.replace('/checkout/contact-data');
-  }
-
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        <CheckoutSummary 
+        <CheckoutSummary
           ingredients={this.state.ingredients}
-          checkoutCancelled = {this.checkoutCanceledHandler}
-          checkoutContinued = {this.checkoutContinuedHandler}
-          />
+          checkoutCancelled={this.checkoutCanceledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route
+          path={this.props.match.url + "/contact-data"}
+          component={ContactData}
+        />
       </div>
-    )
+    );
   }
 }
 
---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Button from "../../../components/UI/Button/Button";
+import classes from "./ContactData.css"
+class ContactData extends Component{
+  state= {
+    name:'',
+    email:'',
+    addredd:{
+      street:'',
+      postalCode:''
+    }
+
+  }
+  render(){
+    return(
+      <div className={classes.ContactData}>
+        <h4>Entry your Contact Data</h4>
+        <form>
+          <input className={classes.Input} type="text" name="name" placeholder="Your name"/>
+          <input className={classes.Input} type="email" name="email" placeholder="Your Mail"/>
+          <input className={classes.Input} type="text" name="street" placeholder="Your Street"/>
+          <input className={classes.Input} type="text" name="postalCode" placeholder="Your postalcode"/>
+          <Button btnType="Success">ORDER</Button>
+        </form>
+      </div>
+    )
+  }
+
+}
+export default ContactData;
