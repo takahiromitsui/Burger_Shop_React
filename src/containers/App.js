f07027712e8b343a9fc2713982e59278b0eaadff
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-02 14:57:34 +0200

    Add switch

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Route} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from "../hoc/Layout/Layout";
 import BurgerBuilder from "../containers/BurgerBuilder/BurgerBuilder";
@@ -11,10 +11,12 @@ class App extends Component {
     return (
       <div>
         <Layout>
+        <Switch>
           <Route path="/checkout" component={Checkout}/>
           <Route path="/orders" component={Orders}/>
           <Route path="/auth" component={Auth}/>
           <Route path="/" exact component={BurgerBuilder}/>
+        </Switch>
         </Layout>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+import { createStore} from 'redux';
 
 import "./index.css";
 import App from "./containers/App";
