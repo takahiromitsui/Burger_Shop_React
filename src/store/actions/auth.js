d50e9b3f8c13e6c0243c146e1f96f835147d258e
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-02 17:18:53 +0200

    Add expire time

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,48 +1,62 @@
-import * as actionTypes from './actionTypes';
 import axios from 'axios';
+
+import * as actionTypes from './actionTypes';
+
 export const authStart = () => {
-	return {
-		type: actionTypes.AUTH_START,
-	};
+    return {
+        type: actionTypes.AUTH_START
+    };
 };
 
-export const authSuccess = authData => {
-	return {
-		type: actionTypes.AUTH_SUCCESS,
-		authData: authData,
-	};
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: token,
+        userId: userId
+    };
 };
 
-export const authFail = error => {
-	return {
-		type: actionTypes.AUTH_FAIL,
-		error: error,
-	};
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    };
 };
 
 export const auth = (email, password, isSignup) => {
-	return dispatch => {
-		dispatch(authStart());
-		const authData = {
-			email: email,
-			password: password,
-			returnSecureToken: true,
-		};
-		let url =
-			'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB0FFw_qfQ-dQt6hdHNlRIRj8MQS95oE28';
-		if (!isSignup) {
-			url =
-				'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB0FFw_qfQ-dQt6hdHNlRIRj8MQS95oE28';
-		}
-		axios
-			.post(url, authData)
-			.then(response => {
-				console.log(response);
-				dispatch(authSuccess(response.data.idToken, response.data.localId));
-			})
-			.catch(err => {
-				console.log(err);
-				dispatch(authFail(err));
-			});
-	};
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB0FFw_qfQ-dQt6hdHNlRIRj8MQS95oE28';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB0FFw_qfQ-dQt6hdHNlRIRj8MQS95oE28';
+        }
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
+            })
+            .catch(err => {
+                dispatch(authFail(err.response.data.error));
+            });
+    };
 };
