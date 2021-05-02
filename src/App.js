ccd2c9fd3b8ba4b535f71e2ed15d7cc3e6a290b9
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-02 18:02:35 +0200

    Add logout

--- src/App.js
+++ src/App.js
@@ -3,25 +3,24 @@ import { Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 
 class App extends Component {
-  render () {
-    return (
-      <div>
-        <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
-        </Layout>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Layout>
+				<Switch>
+					<Route path='/auth' component={Auth} />
+					<Route path='/' exact component={BurgerBuilder} />
+					<Route path='/orders' component={Orders} />
+					<Route path='/logout' component={Logout} />
+					<Route path='/' exact component={BurgerBuilder} />
+				</Switch>
+			</Layout>
+		);
+	}
 }
 
 export default App;
