e73c1ff6d010f455f873679d7556cc993641f8db
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-02 17:05:42 +0200

    Add authentication forms and apis

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -37,56 +37,63 @@ class Auth extends Component {
                 valid: false,
                 touched: false
             }
-        }
+        },
+        isSignup: true
     }
 
-    checkValidity(value, rules) {
+    checkValidity ( value, rules ) {
         let isValid = true;
-        if (!rules) {
+        if ( !rules ) {
             return true;
         }
-        
-        if (rules.required) {
+
+        if ( rules.required ) {
             isValid = value.trim() !== '' && isValid;
         }
 
-        if (rules.minLength) {
+        if ( rules.minLength ) {
             isValid = value.length >= rules.minLength && isValid
         }
 
-        if (rules.maxLength) {
+        if ( rules.maxLength ) {
             isValid = value.length <= rules.maxLength && isValid
         }
 
-        if (rules.isEmail) {
+        if ( rules.isEmail ) {
             const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
+            isValid = pattern.test( value ) && isValid
         }
 
-        if (rules.isNumeric) {
+        if ( rules.isNumeric ) {
             const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
+            isValid = pattern.test( value ) && isValid
         }
 
         return isValid;
     }
 
-    inputChangedHandler = (event, controlName) => {
+    inputChangedHandler = ( event, controlName ) => {
         const updatedControls = {
             ...this.state.controls,
             [controlName]: {
                 ...this.state.controls[controlName],
                 value: event.target.value,
-                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                valid: this.checkValidity( event.target.value, this.state.controls[controlName].validation ),
                 touched: true
             }
         };
-        this.setState({controls: updatedControls});
+        this.setState( { controls: updatedControls } );
     }
 
-    submitHandler = (event) => {
+    submitHandler = ( event ) => {
         event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+        this.props.onAuth( this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup );
+    }
+
+    switchAuthModeHandler = () => {
+        this.setState(prevState => {
+            return {isSignup: !prevState.isSignup};
+        });
     }
 
     render () {
@@ -116,6 +123,9 @@ class Auth extends Component {
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
+                <Button 
+                    clicked={this.switchAuthModeHandler}
+                    btnType="Danger">SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}</Button>
             </div>
         );
     }
@@ -123,8 +133,8 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password))
+        onAuth: ( email, password, isSignup ) => dispatch( actions.auth( email, password, isSignup ) )
     };
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect( null, mapDispatchToProps )( Auth );
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,8 +1,13 @@
 export {
   addIngredient,
-  removeIngredient
+  removeIngredient,
+  initIngredients
 } from './burgerBuilder';
-
 export {
-
+  purchaseBurger,
+  purchaseInit,
+  fetchOrders
 } from './order';
+export {
+  auth
+} from './auth';
