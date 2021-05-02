69d6a537f46abf3ff1fbdb488747e13cfc6f2fd9
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-02 18:36:27 +0200

    Add local storage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -17,6 +17,9 @@ export const authSuccess = (token, userId) => {
 };
 
 export const authFail = (error) => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_FAIL,
         error: error
@@ -52,6 +55,10 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
